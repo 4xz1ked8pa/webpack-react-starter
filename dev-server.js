e30c514c4109361024d539e90da6c6006ccec0f7
Harris Robin Kalash <harrisrobinkalash@gmail.com>
2016-06-26 12:05:46 -0400

    prod webpack

--- dev-server.js
+++ dev-server.js
@@ -3,7 +3,8 @@ var WebPackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config.dev');
 
 new WebPackDevServer(webpack(config), {
-  publicPath: config.output.publicPath
+  publicPath: config.output.publicPath,
+  hot: true
 }).listen(3000, 'localhost', function(err, result) {
   if(err) {
     return console.log(err);
